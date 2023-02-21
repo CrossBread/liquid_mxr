ebbaa70e107e46d9341b820f73833e52cced1f7d
Jeff <jeff.neet@gmail.com>
2023-02-21 04:03:08 -0700

    Basic Pages

---
+++ liquid_mxr/lib/app_widget.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:liquid_mxr/color_mixer/presentation/pages/mixer_page.dart';
+
+class AppWidget extends StatelessWidget {
+  const AppWidget({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MixerPage();
+  }
+}
